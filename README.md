075296f552dcac604e6954948a09d323f33bd698
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2016-09-01 06:41:38 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+```
+npm install
+browserify -t reactify main.js -o bundle.js
+```
+
+open index.html
---
+++ main.js
@@ -0,0 +1,19 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+var Calendar = require('react-input-calendar').default;
+//import Calendar from 'react-input-calendar'
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+
+ReactDom.render(
+    <Calendar format="DD/MM/YYYY" date="4-12-2014" closeOnSelect={true}/>,
+    document.getElementById('content')
+);
